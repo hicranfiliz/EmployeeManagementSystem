99162305e4b0ca2480d808cf1dce48c72b135ddc
hicranfiliz <h.filiz781@gmail.com>
2025-01-22 13:52:25 +0300

    added clientlibrary services and helpers

---
+++ ClientLibrary/Helpers/CustomAuthenticationStateProvider.cs
@@ -0,0 +1,13 @@
+﻿
+using Microsoft.AspNetCore.Components.Authorization;
+
+namespace ClientLibrary.Helpers;
+
+public class CustomAuthenticationStateProvider : AuthenticationStateProvider
+{
+    // bir sayfadan digerine gecildiginde kimlik dogrulamasinin yapilip yapilmadigi kontrol edilecek.
+    public override Task<AuthenticationState> GetAuthenticationStateAsync()
+    {
+        throw new NotImplementedException();
+    }
+}
