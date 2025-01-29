a08846e2c2fde177d039bcbfa46b4f89063ef57b
hicranfiliz <h.filiz781@gmail.com>
2025-01-29 12:11:22 +0300

    added employee base models and their hierarchies

---
+++ BaseLibrary/Entities/Sanction.cs
@@ -0,0 +1,17 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.Entities;
+
+public class Sanction : OtherBaseEntity
+{
+    [Required]
+    public DateTime Date { get; set; }
+    [Required]
+    public string Punishment { get; set; }
+    [Required]
+    public DateTime PunishmentDate { get; set; }
+
+    // many to one relationship with Vacation Type
+    public SanctionType? SanctionType { get; set; }
+}
---
+++ BaseLibrary/Entities/Vacation.cs
@@ -0,0 +1,20 @@
+﻿
+using System.ComponentModel.DataAnnotations;
+
+namespace BaseLibrary.Entities
+{
+    public class Vacation : OtherBaseEntity
+    {
+        [Required]
+        public DateTime StartDate { get; set; }
+        [Required] public int NumberOfDays { get; set; }
+        [Required] 
+        public DateTime EndDate => StartDate.AddDays(NumberOfDays);
+
+        //Many to one relationship with VacationType
+        public VacationType? VacationType { get; set; }
+        [Required]
+        public int VacationTypeId { get; set; }
+        
+    }
+}
