ad0811189d7ea421aff55c7bffd3cf149a58b02f
hicranfiliz <h.filiz781@gmail.com>
2025-01-29 16:26:36 +0300

    added generic repository interface and general department repo

---
+++ ServerLibarry/Repositories/Implementations/GeneralDepartmentRepository.cs
@@ -0,0 +1,33 @@
+﻿
+using BaseLibrary.Responses;
+using ServerLibarry.Repositories.Contracts;
+
+namespace ServerLibarry.Repositories.Implementations;
+
+public class GeneralDepartmentRepository : IGenericRepositoryInterface<GeneralResponse>
+{
+    public Task<GeneralResponse> DeleteById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<List<GeneralResponse>> GetAll()
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GeneralResponse> GetById(int id)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GeneralResponse> Insert(GeneralResponse item)
+    {
+        throw new NotImplementedException();
+    }
+
+    public Task<GeneralResponse> Update(GeneralResponse item)
+    {
+        throw new NotImplementedException();
+    }
+}
