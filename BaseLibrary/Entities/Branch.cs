53d29ed1f9854f45a76b08056762c32562350f1c
hicranfiliz <h.filiz781@gmail.com>
2025-01-29 11:21:48 +0300

    added department and country base model hierarchy

--- BaseLibrary/Entities/Branch.cs
+++ BaseLibrary/Entities/Branch.cs
@@ -3,5 +3,11 @@ namespace BaseLibrary.Entities
 {
     public class Branch : BaseEntity
     {
+        //Many to one relationship with Department
+        public Department? Department { get; set; }
+        public int DepartmentId { get; set; }
+
+        // One to many relationship with Employees
+        public List<Employee>? Employees { get; set; }
     }
 }
--- BaseLibrary/Entities/Employee.cs
+++ BaseLibrary/Entities/Employee.cs
@@ -1,21 +1,28 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
+
 namespace BaseLibrary.Entities;
 
 public class Employee : BaseEntity
 {
-    public string? CivilId { get; set; }
-    public string? FileNumber { get; set; }
-    public string? FullName { get; set; }
-    public string? JobName { get; set; }
-    public string? Address { get; set; }
-    public string? TelephoneNumber { get; set; }
-    public string? Photo { get; set; }
+    [Required]
+    public string CivilId { get; set; } = string.Empty;
+    [Required]
+    public string FileNumber { get; set; } = string.Empty;
+    [Required]
+    public string FullName { get; set; } = string.Empty;
+    [Required]
+    public string JobName { get; set; } = string.Empty;
+    [Required]
+    public string Address { get; set; } = string.Empty;
+    [Required, DataType(DataType.PhoneNumber)]
+    public string TelephoneNumber { get; set; } = string.Empty;
+    [Required]
+    public string Photo { get; set; } = string.Empty;
     public string? Other { get; set; }
 
-    //relationship: Many to one
-    public GeneralDepartment? GeneralDepartment { get; set; }
-    public Department? Department { get; set; }
-    public int DepartmentId { get; set; }
+    //relationship: Many to one with Branch
+
     public Branch? Branch { get; set; }
     public int BranchId { get; set; }
     public Town? Town { get; set; }
