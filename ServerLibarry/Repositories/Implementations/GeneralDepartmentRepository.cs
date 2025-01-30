3060fd68aae09bfe168f904298923660d811efc6
hicranfiliz <h.filiz781@gmail.com>
2025-01-30 10:36:32 +0300

    created department repository

--- ServerLibarry/Repositories/Implementations/GeneralDepartmentRepository.cs
+++ ServerLibarry/Repositories/Implementations/GeneralDepartmentRepository.cs
@@ -46,7 +46,7 @@ public class GeneralDepartmentRepository(AppDbContext appDbContext) : IGenericRe
 
     private async Task<bool> CheckName(string name)
     {
-        var item = await appDbContext.Departments.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
+        var item = await appDbContext.GeneralDepartments.FirstOrDefaultAsync(x => x.Name!.ToLower().Equals(name.ToLower()));
         return item is null;
     }
 }
