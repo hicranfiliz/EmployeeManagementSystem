f12f6b4559fbfadb21e96c5468ca79fb53da1d47
hicranfiliz <h.filiz781@gmail.com>
2025-01-31 22:33:37 +0300

    created country controller

--- Server/Controllers/CountryController.cs
+++ Server/Controllers/CountryController.cs
@@ -1,4 +1,5 @@
 ﻿using BaseLibrary.Entities;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using ServerLibarry.Repositories.Contracts;
 
@@ -6,8 +7,8 @@ namespace Server.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class BranchtController(IGenericRepositoryInterface<Branch> genericRepositoryInterface)
-        : GenericController<Branch>(genericRepositoryInterface)
+    public class CountryController(IGenericRepositoryInterface<Country> genericRepositoryInterface)
+        : GenericController<Country>(genericRepositoryInterface)
     {
     }
 }
